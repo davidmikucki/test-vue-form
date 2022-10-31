7cd36dc31cef90d6e68f554952217d7c1fdb03ca
David Mikucki <info@relight.app>
2022-10-31 11:05:33 -0600

    First commit.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,205 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 class="siteTitle">Forms-R-Us</h1>
+    <section class="content">
+      <webform
+        title="Send a Friend Some Live-Badgers®"
+        form-id="badgerForm"
+        :fields="badgerFormFields"
+        submitText="Badger ’Em!"
+        :submitAction="badgerEm"
+        icon="bx:shocked"
+      />
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Webform from "./components/Webform.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Webform },
+  methods: {
+    badgerEm() {
+      let string = "Your form data! \n";
+      this.badgerFormFields.forEach((element) => {
+        string += element.id + ": " + element.value + "\n";
+      });
+      alert(string)
+      //alert(string);
+    },
+  },
+  data() {
+    return {
+      badgerFormFields: [
+        {
+          type: "textInput",
+          value: "",
+          id: "senderName",
+          variableName: "senderNameValue",
+          title: "Your Full Name",
+          placeholder: "ex. John Doe",
+          required: true,
+        },
+        {
+          type: "select",
+          value: '',
+          id: "badgerCount",
+          variableName: "badgerCountValue",
+          title: "Number of Badgers",
+          options: [
+            {
+              value: 1,
+              name: "One",
+            },
+            {
+              value: 2,
+              name: "Two",
+            },
+            {
+              value: 3,
+              name: "Three",
+            },
+            {
+              value: 4,
+              name: "Four",
+            },
+            {
+              value: 5,
+              name: "Five",
+            },
+            {
+              value: 6,
+              name: "Six",
+            },
+            {
+              value: 7,
+              name: "Seven",
+            },
+            {
+              value: 8,
+              name: "Eight",
+            },
+          ],
+          required: true,
+        },
+        {
+          type: "radioButtons",
+          id: "hungriness",
+          variableName: "hungrinessValue",
+          title: "How Hungry Should the Badgers Be?",
+          required: true,
+          value: "",
+          options: [
+            { title: "Ravenous", name: "ravenous" },
+            { title: "A Mite Peckish", name: "peckish" },
+            { title: "Still Quite Full From Last Meal", name: "full" },
+          ],
+        },
+        {
+          type: "checkboxes",
+          id: "otherShippables",
+          variableName: "otherShippablesValue",
+          title: "What Else Should We Include?",
+          required: false,
+          value: [],
+          options: [
+            { title: "Band-Aids", name: "bandaids" },
+            { title: "Tylenol", name: "tylenol" },
+            { title: "Glitter", name: "glitter" },
+          ],
+        },
+        {
+          type: "textInput",
+          value: "",
+          id: "recipientName",
+          variableName: "recipientNameValue",
+          title: "Victim Full Name",
+          placeholder: "ex. James McBadgered",
+          required: true,
+        },
+        {
+          type: "textInput",
+          value: "",
+          id: "street1",
+          variableName: "street1Value",
+          title: "Street",
+          placeholder: "ex. 1234 Badger Lane",
+          required: true,
+        },
+        {
+          type: "textInput",
+          value: "",
+          id: "street2",
+          variableName: "street2Value",
+          title: "Street 2",
+          placeholder: "ex. Apt. D",
+          required: false,
+        },
+        {
+          type: "textInput",
+          value: "",
+          id: "city",
+          variableName: "cityValue",
+          title: "City",
+          placeholder: "ex. New Badger City",
+          required: true,
+        },
+        {
+          type: "textInput",
+          value: "",
+          id: "state",
+          variableName: "stateValue",
+          title: "State",
+          placeholder: "ex. Pennsylvania",
+          required: true,
+        },
+        {
+          type: "textInput",
+          value: "",
+          id: "zipcode",
+          variableName: "zipcode",
+          title: "Zip Code",
+          placeholder: "ex. 19019",
+          required: true,
+        },
+      ],
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+@import "~normalize.css";
+@import url("https://fonts.googleapis.com/css2?family=Outfit:wght@100;200;500;800&display=swap");
+html {
+  font-size: 18px;
+  body {
+    font-family: "outfit", "avenir", sans-serif;
+    font-weight: 200;
+    font-size: 1em;
+    background: #0d3b66;
+    box-sizing: border-box;
+    color: #222;
+    line-height: 1.65;
+    * {
+      box-sizing: border-box;
+    }
+    h1.siteTitle {
+      text-align: center;
+      font-weight: 800;
+      font-size: 3em;
+      color: #0d3b66;
+      background: #f4d35e;
+      margin-top: 0;
+      padding: 0.25em;
+      box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.42);
+    }
+    section.content {
+      display: flex;
+      padding: 0 0.5em;
+      justify-content: center;
+    }
+  }
 }
 </style>
---
+++ src/components/Webform.vue
@@ -0,0 +1,226 @@
+<template>
+  <div class="webform">
+    <form :id="formId" @submit.prevent="submitForm(formId)" novalidate>
+      <h2>{{ title }}</h2>
+      <div class="error" v-show="errorState">
+        <p>
+          It looks like you may have forgotten something. Please make sure you
+          filled out all of the required fields.
+        </p>
+      </div>
+      <div
+        class="field"
+        :class="{
+          required: field.required,
+          hasError: field.required && field.value.length == 0 && errorState,
+        }"
+        v-for="(field, i) in fields"
+        :key="i"
+      >
+        <label class="wholeField" :for="field.id">{{ field.title }}</label>
+        <input
+          type="text"
+          v-if="field.type == 'textInput'"
+          :id="field.id"
+          :ref="field.id"
+          v-model="field.value"
+          :required="field.required"
+          :placeholder="field.placeholder"
+        />
+        <select
+          v-else-if="field.type == 'select'"
+          :id="field.id"
+          :ref="field.id"
+          v-model="field.value"
+          :required="field.required"
+        >
+          <option
+            v-for="option in field.options"
+            :key="option.value"
+            :value="option.value"
+          >
+            {{ option.name }}
+          </option>
+        </select>
+
+        <div class="radioButtons" v-else-if="field.type == 'radioButtons'">
+          <div
+            class="radioButton option"
+            v-for="button in field.options"
+            :key="button.value"
+          >
+            <input
+              type="radio"
+              :id="button.name"
+              :ref="button.name"
+              :required="button.required"
+              :name="field.variableName"
+              v-model="field.value"
+              :value="button.name"
+            />
+            <label class="radioLabel" :for="button.name">{{
+              button.title
+            }}</label>
+          </div>
+        </div>
+        <div class="checkboxes" v-else-if="field.type == 'checkboxes'">
+          <div
+            class="checkbox option"
+            v-for="button in field.options"
+            :key="button.value"
+          >
+            <input
+              type="checkbox"
+              :id="button.name"
+              :ref="button.name"
+              :required="button.required"
+              :name="field.variableName"
+              v-model="field.value"
+              :value="button.name"
+            />
+            <label class="checkboxLabel" :for="button.name">{{
+              button.title
+            }}</label>
+          </div>
+        </div>
+      </div>
+      <button type="submit" :form="formId">
+        <Icon v-if="icon" :icon="icon" /> {{ submitText }}
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { Icon } from "@iconify/vue2";
+export default {
+  name: "Webform",
+  components: { Icon },
+  props: {
+    title: String,
+    formId: String,
+    fields: Array,
+    icon: String,
+    submitText: String,
+    submitAction: Function,
+  },
+  data() {
+    return {
+      errorState: false,
+    };
+  },
+  methods: {
+    submitForm() {
+      let foundError = false;
+      this.fields.forEach((field) => {
+        if (field.required && field.value.length == 0) {
+          foundError = true;
+        }
+      });
+      if (foundError) {
+        window.scrollTo({ top: 0, behavior: "smooth" });
+        this.errorState = true;
+      } else {
+        this.submitAction();
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.webform {
+  width: 100%;
+  max-width: 560px;
+  border-radius: 12px;
+  background: #fff;
+  padding: 1.5em;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.42);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 2em;
+  h2 {
+    margin-top: 0;
+    font-weight: 400;
+    color: #0d3b66;
+    margin-bottom: 0.5em;
+    text-align: center;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .field {
+      margin: 0.75em 0;
+      width: 100%;
+      max-width: 340px;
+      transition: all ease 300ms;
+      &.required {
+        &.hasError {
+          background: #f95738;
+          color: #fff;
+          padding: 1em;
+          border-radius: 12px;
+          padding-bottom: 1.5em;
+          label.wholeField::after {
+            color: #fff;
+            content: " – This field is required.";
+            font-size: 0.75em;
+          }
+        }
+        label.wholeField::after {
+          content: " *";
+          color: #f95738;
+        }
+      }
+      label.wholeField {
+        display: block;
+        margin-bottom: 0.25em;
+        font-weight: 400;
+      }
+      input[type="text"] {
+        font-weight: 200;
+        width: 100%;
+        padding: 0.25em;
+      }
+      select {
+        font-weight: 300;
+      }
+      .option {
+        input {
+          margin-right: 0.25em;
+        }
+      }
+    }
+    .error {
+      color: #f95738;
+
+      p {
+        margin-top: 0;
+        font-weight: 400;
+      }
+      ul {
+        margin: 0.5em 0 0 0;
+      }
+    }
+    button {
+      background: #f4d35e;
+      border: none;
+      border-radius: 12px;
+      padding: 0.5em 1em;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      font-weight: 500;
+      width: 100%;
+      max-width: 340px;
+      text-align: center;
+      margin-top: 1.5em;
+      svg {
+        margin-right: 5px;
+      }
+    }
+  }
+}
+</style>
